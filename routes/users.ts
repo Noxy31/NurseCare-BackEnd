fd0162497f3817dcb437de21d3f419aaaefa9fe8
Noxy31 <nellenoar@gmail.com>
2025-01-13 16:29:29 +0100

    updated postgre sql

--- routes/users.ts
+++ routes/users.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import { authMiddleware, CustomRequest } from "../middlewares/authenticate";
 import bcrypt from "bcrypt";
 import { query } from "../db";
-
 dotenv.config();
 
 const usersRouter = Router();
@@ -11,16 +10,13 @@ const usersRouter = Router();
 usersRouter.post(
   "/create-user",
   authMiddleware,
-
   async (req: Request, res: Response) => {
     const { userName, userMail, userPass, userRole } = req.body;
-
     try {
       const hashedPassword = await bcrypt.hash(userPass, 10);
       const sql =
-        "INSERT INTO users (userName, userMail, userPass, userRole) VALUES (?, ?, ?, ?)";
+        'INSERT INTO "users" ("userName", "userMail", "userPass", "userRole") VALUES ($1, $2, $3, $4)';
       await query(sql, [userName, userMail, hashedPassword, userRole]);
-
       res.status(201).json({ message: "User successfully created" });
     } catch (error) {
       res.status(500).send(error);
@@ -33,9 +29,8 @@ usersRouter.get(
   authMiddleware,
   async (req: Request, res: Response) => {
     try {
-      const sql = "SELECT idUser, userName, userMail, userRole FROM users";
+      const sql = 'SELECT "idUser", "userName", "userMail", "userRole" FROM "users"';
       const users = await query(sql);
-
       res.status(200).json(users);
     } catch (error) {
       res.status(500).send(error);
@@ -48,15 +43,12 @@ usersRouter.post(
   authMiddleware,
   async (req: CustomRequest, res: Response) => {
     const { idUser } = req.body;
-
     try {
       if (!idUser) {
         res.status(400).send("L'identifiant de l'utilisateur est requis.");
       }
-
-      const sql = "DELETE FROM users WHERE idUser = ?";
+      const sql = 'DELETE FROM "users" WHERE "idUser" = $1';
       await query(sql, [idUser]);
-
       res.status(200).json({ message: "Utilisateur supprimé avec succès" });
     } catch (error) {
       res.status(500).send(error);
@@ -69,18 +61,15 @@ usersRouter.put(
   authMiddleware,
   async (req: Request, res: Response) => {
     const { idUser, userName, userMail, userRole } = req.body;
-
     try {
       if (!idUser) {
         res.status(400).send("User ID is required.");
         console.log("IdUser not found.");
         return;
       }
-
       const sql =
-        "UPDATE users SET userName = ?, userMail = ?, userRole = ? WHERE idUser = ?";
+        'UPDATE "users" SET "userName" = $1, "userMail" = $2, "userRole" = $3 WHERE "idUser" = $4';
       await query(sql, [userName, userMail, userRole, idUser]);
-
       res.status(200).json({ message: "User Successfully updated" });
     } catch (error) {
       console.log("Error updating user" + error);
@@ -94,7 +83,7 @@ usersRouter.get(
   authMiddleware,
   async (req: Request, res: Response) => {
     try {
-      const sql = "SELECT idUser, userName FROM users WHERE userRole = ?";
+      const sql = 'SELECT "idUser", "userName" FROM "users" WHERE "userRole" = $1';
       const nurses = await query(sql, [3]);
       res.status(200).json(nurses);
     } catch (error) {
