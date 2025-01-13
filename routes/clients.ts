01096e0464bdfbe211b3c6b6924244f63ba04e1e
Noxy31 <nellenoar@gmail.com>
2025-01-13 16:47:00 +0100

    modified route for postgre

--- routes/clients.ts
+++ routes/clients.ts
@@ -1,10 +1,8 @@
 import { Router, Request, Response } from "express";
 import dotenv from "dotenv";
 import authMiddleware from "../middlewares/authenticate";
-// import isAdminMiddleware from "../middlewares/isadmin";
 import { CustomRequest } from "../middlewares/authenticate";
 import { query } from "../db";
-
 dotenv.config();
 
 const clientRouter = Router();
@@ -15,7 +13,7 @@ clientRouter.get(
   authMiddleware,
   async (req: Request, res: Response) => {
     try {
-      const sql = "SELECT idClient, clientName FROM clients";
+      const sql = 'SELECT "idClient", "clientName" FROM "clients"';
       const clients = await query(sql);
       res.status(200).json(clients);
     } catch (error) {
@@ -25,31 +23,30 @@ clientRouter.get(
 );
 
 clientRouter.get(
-    "/get-clients",
-    authMiddleware,
-    async (req: Request, res: Response) => {
-      try {
-        const sql = "SELECT idClient, clientName, clientMail, clientAddress, clientPhone FROM clients";
-        const clients = await query(sql);
-  
-        res.status(200).json(clients);
-      } catch (error) {
-        res.status(500).send(error);
-      }
+  "/get-clients",
+  authMiddleware,
+  async (req: Request, res: Response) => {
+    try {
+      const sql =
+        'SELECT "idClient", "clientName", "clientMail", "clientAddress", "clientPhone" FROM "clients"';
+      const clients = await query(sql);
+      res.status(200).json(clients);
+    } catch (error) {
+      res.status(500).send(error);
     }
-  );
+  }
+);
 
 clientRouter.post(
   "/create-client",
   authMiddleware,
   async (req: Request, res: Response) => {
     const { clientName, clientAddress, clientPhone, clientMail } = req.body;
-
     try {
       const sql = `
-          INSERT INTO clients (clientName, clientAddress, clientPhone, clientMail) 
-          VALUES (?, ?, ?, ?)
-        `;
+        INSERT INTO "clients" ("clientName", "clientAddress", "clientPhone", "clientMail")
+        VALUES ($1, $2, $3, $4)
+      `;
       await query(sql, [clientName, clientAddress, clientPhone, clientMail]);
       res.status(201).json({ message: "Patient successfully created" });
     } catch (error) {
@@ -62,24 +59,17 @@ clientRouter.put(
   "/update-client",
   authMiddleware,
   async (req: Request, res: Response) => {
-    const {
-      idClient,
-      clientName,
-      clientAddress,
-      clientPhone,
-      clientMail
-    } = req.body;
-
+    const { idClient, clientName, clientAddress, clientPhone, clientMail } =
+      req.body;
     try {
       if (!idClient) {
         res.status(400).json({ error: "Client id is required" });
       }
-
       const sql = `
-          UPDATE clients 
-          SET clientName = ?, clientAddress = ?, clientPhone = ?, clientMail = ?
-          WHERE idClient = ?
-        `;
+        UPDATE "clients"
+        SET "clientName" = $1, "clientAddress" = $2, "clientPhone" = $3, "clientMail" = $4
+        WHERE "idClient" = $5
+      `;
       await query(sql, [
         clientName,
         clientAddress,
@@ -95,30 +85,26 @@ clientRouter.put(
 );
 
 clientRouter.post(
-    "/delete-client",
-    authMiddleware,
-    async (req: Request, res: Response) => {
-      const { idAppointment } = req.body;
-  
-      try {
-        if (!idAppointment) {
-          res
-            .status(400)
-            .json({ error: "Client id is required" });
-        }
-  
-        const sql = `
-          DELETE FROM clients 
-          WHERE idAppointment = ?
-        `;
-        await query(sql, [idAppointment]);
-        res.status(200).json({ message: "Appointment successfully deleted" });
-      } catch (error) {
-        res
-          .status(500)
-          .json({ error: "Error deleting appointment" });
+  "/delete-client",
+  authMiddleware,
+  async (req: Request, res: Response) => {
+    const { idClient } = req.body;
+
+    try {
+      if (!idClient) {
+        res.status(400).json({ error: "Client id is required" });
       }
+
+      const sql = `
+  DELETE FROM "clients"
+  WHERE "idClient" = $1
+`;
+      await query(sql, [idClient]);
+      res.status(200).json({ message: "Appointment successfully deleted" });
+    } catch (error) {
+      res.status(500).json({ error: "Error deleting appointment" });
     }
-  );
+  }
+);
 
 export default clientRouter;
